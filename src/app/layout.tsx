0c765fe0fc4baa8a439aae8cd2d18ebcb44dac12
onurcete <ceteonur@gmail.com>
2026-06-17 23:09:19 +0300

    feat: set dark mode as default on first load

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,16 +21,16 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="tr" className={`${inter.variable}`}>
+    <html lang="tr" className={`dark ${inter.variable}`}>
       <head>
         <script
           dangerouslySetInnerHTML={{
             __html: `
               try {
-                if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
-                  document.documentElement.classList.add('dark');
-                } else {
+                if (localStorage.theme === 'light') {
                   document.documentElement.classList.remove('dark');
+                } else {
+                  document.documentElement.classList.add('dark');
                 }
               } catch (_) {}
             `,
